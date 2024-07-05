1b18b2d41fe26ad73f7f3fdb45f8baa4f66f0da9
ramilouati <ramilouati16@gmail.com>
2024-07-05 17:30:59 +0100

    EZRT

--- src/data/brands.js
+++ src/data/brands.js
@@ -1,24 +1,16 @@
 export const brandImages = [
-  { id: 1, src: "/assets/images/brands/brand1.png", width: 120, height: 20 },
   { id: 3, src: "/assets/images/brands/brand3.png", width: 132, height: 22 },
   { id: 4, src: "/assets/images/brands/brand4.png", width: 72, height: 21 },
-  { id: 5, src: "/assets/images/brands/brand5.png", width: 123, height: 31 },
   { id: 6, src: "/assets/images/brands/brand6.png", width: 137, height: 22 },
-  { id: 1, src: "/assets/images/brands/brand1.png", width: 120, height: 20 },
   { id: 3, src: "/assets/images/brands/brand3.png", width: 132, height: 22 },
   { id: 4, src: "/assets/images/brands/brand4.png", width: 72, height: 21 },
-  { id: 5, src: "/assets/images/brands/brand5.png", width: 123, height: 31 },
   { id: 6, src: "/assets/images/brands/brand6.png", width: 137, height: 22 },
 ];
 export const brandImages2 = [
-  { id: 1, src: "/assets/images/brands/brand1.png", width: 120, height: 20 },
   { id: 3, src: "/assets/images/brands/brand3.png", width: 132, height: 22 },
   { id: 4, src: "/assets/images/brands/brand4.png", width: 72, height: 21 },
-  { id: 5, src: "/assets/images/brands/brand5.png", width: 123, height: 31 },
   { id: 6, src: "/assets/images/brands/brand6.png", width: 137, height: 22 },
-  { id: 1, src: "/assets/images/brands/brand1.png", width: 120, height: 20 },
   { id: 3, src: "/assets/images/brands/brand3.png", width: 132, height: 22 },
   { id: 4, src: "/assets/images/brands/brand4.png", width: 72, height: 21 },
-  { id: 5, src: "/assets/images/brands/brand5.png", width: 123, height: 31 },
   { id: 6, src: "/assets/images/brands/brand6.png", width: 137, height: 22 },
 ];
